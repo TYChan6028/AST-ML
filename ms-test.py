016ad1de46fbd85d7eb7023b253badfe7c65d28d
TYChan6028 <F04066028@gs.ncku.edu.tw>
2020-07-23 14:55:01 +0800

    Grab all filenames from MS raw_2018

--- ms-test.py
+++ ms-test.py
@@ -1,13 +1,21 @@
 from pyopenms import *
+import os
 
-exp = MSExperiment()
-MzMLFile().load("18-01-01_Labo1_0AAADLZ_1_0101DS172207384_L4_1117_1.mzML", exp)
+# exp = MSExperiment()
+# MzMLFile().load("18-01-01_Labo1_0AAADLZ_1_0101DS172207384_L4_1117_1.mzML", exp)
 
-spec = exp[0]
-mz, intensity = spec.get_peaks()
+# spec = exp[0]
+# mz, intensity = spec.get_peaks()
 
-print(mz)
-print(len(mz))
-print()
-print(intensity)
-print(len(intensity))
+# print(mz)
+# print(len(mz))
+# print()
+# print(intensity)
+# print(len(intensity))
+
+os.chdir('/Users/ethanchan/AST-ML/ms-data/MS raw_2018/')
+# file = '201710-201911generated_id_ast_export.csv'
+filenames = os.listdir()
+filenames.sort()
+print(filenames)
+# print("Target file is: ", file)
