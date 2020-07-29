a559152c13852d49a5274b04da9c883c3ce9d232
TYChan6028 <F04066028@gs.ncku.edu.tw>
2020-07-29 16:51:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import ast_functions as ast
-import ms_test as test
+# import ms_test as test
 
 
 # run this to load the cleaned ast dataset
 content = ast.load_cleaned_record()
 print("length of content:", len(content))
+id_name_dict, content = ast.match_id_w_filename(content)
+ast.load_ms_data('3585071', id_name_dict)
