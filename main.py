152a6ae3756a164c63ac8c5e98ee0ae380c35447
TYChan6028 <F04066028@gs.ncku.edu.tw>
2020-07-30 09:15:41 +0800

    Change varname id_fname_pair to id_fname_dict

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ import ast_functions as ast
 # run this to load the cleaned ast dataset
 content = ast.load_cleaned_record()
 print("length of content:", len(content))
-id_fname_pair, content = ast.match_id_w_filename(content)
-print("length of id_fname_pair:", len(id_fname_pair))
-ast.load_ms_data('4228289', id_fname_pair)
+id_fname_dict, content = ast.match_id_w_filename(content)
+print("length of id_fname_dict:", len(id_fname_dict))
+ast.load_ms_data('4228289', id_fname_dict)
 # import json
 # import os
 # os.chdir('/Users/ethanchan/AST-ML/')
-# with open('id_fname_pair.json', 'w') as f:
-#     json.dump(id_fname_pair, f, indent=2)
+# with open('id_fname_dict.json', 'w') as f:
+#     json.dump(id_fname_dict, f, indent=2)
 
-# with open('id_fname_pair.json') as f:
+# with open('id_fname_dict.json') as f:
 #     new_dict = json.load(f)
 
 # print(type(new_dict))
