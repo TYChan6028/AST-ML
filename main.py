c16c0b34a169fb0ae98aab1f32adecc1c2a3b3de
TYChan6028 <F04066028@gs.ncku.edu.tw>
2020-07-30 09:04:24 +0800

    Add dict-json i/o test script

--- main.py
+++ main.py
@@ -5,5 +5,17 @@ import ast_functions as ast
 # run this to load the cleaned ast dataset
 content = ast.load_cleaned_record()
 print("length of content:", len(content))
-id_name_dict, content = ast.match_id_w_filename(content)
-ast.load_ms_data('3585071', id_name_dict)
+id_fname_pair, content = ast.match_id_w_filename(content)
+print("length of id_fname_pair:", len(id_fname_pair))
+ast.load_ms_data('4228289', id_fname_pair)
+# import json
+# import os
+# os.chdir('/Users/ethanchan/AST-ML/')
+# with open('id_fname_pair.json', 'w') as f:
+#     json.dump(id_fname_pair, f, indent=2)
+
+# with open('id_fname_pair.json') as f:
+#     new_dict = json.load(f)
+
+# print(type(new_dict))
+# print(len(new_dict))
