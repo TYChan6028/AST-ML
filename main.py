54427342dc2e14c198993d55d79b056deb235d04
TYChan6028 <F04066028@gs.ncku.edu.tw>
2020-08-03 00:51:28 +0800

    Update main.py with get_sparsed_peak()

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import ast_functions as ast
 import ms_functions as ms
 
 # run this to load the cleaned ast dataset
-content = ast.load_cleaned_record()
-a, b, c, d = ast.get_s_r_ratio(content)
-print('s', a, c)
-print('r', b, d)
+# content = ast.load_cleaned_record()
 # print("length of content:", len(content))
-id_fname_dict = ast.import_id_fname_dict()
+# a, b, c, d = ast.get_s_r_ratio(content)
+# print('s', a, c)
+# print('r', b, d)
+
+# id_fname_dict = ast.import_id_fname_dict()
 # mz, intensity = ms.load_ms_data('3514457', id_fname_dict)
 # print(mz)
 # print(len(mz))
@@ -15,23 +16,8 @@ id_fname_dict = ast.import_id_fname_dict()
 # print(intensity)
 # print(len(intensity))
 
-# mz1, intensity1 = ms.load_ms_data('3514457')
-# mz2, intensity2 = ms.load_ms_data('3536784')
-# print(mz2)
-# print(len(mz2))
-# print()
-# print(intensity2)
-# print(len(intensity2))
-# print(type(mz))
-# print(type(intensity))
-
 # ms.export_ms_data('3514457', mz, intensity)
-ms.import_ms_data('3514457', id_fname_dict)
 
-# # DANGER!!!
-# i = 1
-# for lab_id in id_fname_dict:
-#     mz, intensity = ms.load_ms_data(lab_id, id_fname_dict)
-#     ms.export_ms_data(lab_id, mz, intensity)
-#     print(i)
-#     i += 1
+s_id, r_id = ast.get_s_r_id()
+for lab_id in s_id:
+    sparse_peak = ms.get_sparsed_peak(lab_id)
