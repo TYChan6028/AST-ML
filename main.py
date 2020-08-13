2736736e1fcbc677b82b0ede0d7f0faf84ac866e
TYChan6028 <F04066028@gs.ncku.edu.tw>
2020-08-13 10:21:32 +0800

    Comment out code block for generating id lists for S and R s.aureus

--- main.py
+++ main.py
@@ -18,15 +18,15 @@ import ms_functions as ms
 
 # ms.export_ms_data('3514457', mz, intensity)
 
-s_id, r_id = ast.get_s_r_id()
-import os
-import json
-from numpy import zeros, array
-os.chdir('/Users/ethanchan/AST-ML/')
-l1 = []
-for i in range(len(r_id)):
-    l1.append(i)
-zero = zeros((len(r_id),), dtype=int)
-content = dict(zip(list(r_id), l1))
-with open('r.json', 'w') as f:
-    json.dump(content, f, indent=2)
+# s_id, r_id = ast.get_s_r_id()
+# import os
+# import json
+# from numpy import zeros, array
+# os.chdir('/Users/ethanchan/AST-ML/')
+# l1 = []
+# for i in range(len(r_id)):
+#     l1.append(i)
+# zero = zeros((len(r_id),), dtype=int)
+# content = dict(zip(list(r_id), l1))
+# with open('r.json', 'w') as f:
+#     json.dump(content, f, indent=2)
